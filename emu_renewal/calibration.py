21b960d7b32b8baf1eb8bc34f53ec1b3d3647549
James Trauer <james.trauer@monash.edu>
2024-09-25 15:52:50 +1000

    Replicate the number of dispersion SDs to be one for each target

--- emu_renewal/calibration.py
+++ emu_renewal/calibration.py
@@ -39,10 +39,10 @@ class Calibration:
             self.data[ind] = jnp.array(ind_data.loc[common_dates_idx])
             common_abs_indices = np.array(self.epi_model.epoch.dti_to_index(common_dates_idx).astype(int))
             self.common_indices[ind] = common_abs_indices - self.epi_model.model_times[0]
+            self.data_disp_sd[ind] = 0.1
 
         self.priors = priors
         _ = [p.mean for p in self.priors.values()]  # Compile transformed dists first to avoid memory leaks
-
 
     def calibration(self):
         pass
@@ -66,7 +66,6 @@ class StandardCalib(Calibration):
             data: The data targets
         """
         super().__init__(epi_model, priors, data)
-        self.data_disp_sd = 0.1
         self.proc_disp_sd = 0.1
         self.fixed_params = fixed_params
 
@@ -117,7 +116,7 @@ class StandardCalib(Calibration):
     def add_factor(self, params, indicator):
         log_result = jnp.log(self.get_model_indicator(params, indicator))
         log_target = jnp.log(self.data[indicator])
-        dispersion = numpyro.sample(f"dispersion_{indicator}", dist.HalfNormal(self.data_disp_sd))
+        dispersion = numpyro.sample(f"dispersion_{indicator}", dist.HalfNormal(self.data_disp_sd[indicator]))
         likelihood_contribution = dist.Normal(log_result, dispersion).log_prob(log_target).sum()
         numpyro.factor(f"{indicator}_ll", likelihood_contribution)
 
@@ -128,7 +127,7 @@ class StandardCalib(Calibration):
             "from the end of the run-in phase through to the end of the analysis. "
             "The dispersion parameter for this comparison of log values is also calibrated, "
             "with the dispersion parameter prior using a half-normal distribution, "
-            f"with a standard deviation of {self.data_disp_sd}. "
+            f"with a standard deviation of {self.data_disp_sd[indicator]}. "
         )
 
     def get_description(self) -> str:
