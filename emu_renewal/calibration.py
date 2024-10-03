9ffd632c95979750708535ac3336a98ab63335e8
James Trauer <james.trauer@monash.edu>
2024-10-04 09:31:14 +1000

    Correct typing

--- emu_renewal/calibration.py
+++ emu_renewal/calibration.py
@@ -56,7 +56,7 @@ class StandardCalib(Calibration):
         self,
         epi_model: RenewalModel,
         priors: Dict[str, dist.Distribution],
-        data: pd.Series,
+        data: Dict[str, ],
         data_sds: Dict[str, float],
         fixed_params: Dict[str, float]={},
     ):
