6ce304b8efd59d1a31724735656d3d294a0dbc70
David Shipman <dshipman@gmail.com>
2024-10-16 15:30:26 +1100

    Add generic dispersion target class and update Malaysia notebook

--- emu_renewal/targets.py
+++ emu_renewal/targets.py
@@ -3,28 +3,11 @@ from jax import Array, numpy as jnp
 from numpyro import distributions as dist
 import numpyro
 
+
 class Target:
     data: pd.Series
-    sd: float
     key: str
     calibration_data: Array
-
-    def set_key(self, key):
-        raise(NotImplementedError)
-    
-    def set_calibration_data(self, data):
-        raise NotImplementedError
-    
-    def loglikelihood(self, modelled):
-        raise NotImplementedError
-
-class LogLogHalfNormalTarget(Target):
-    def __init__(self, data: pd.Series, sd: float):
-        self.data = data
-        self.sd = sd
-        
-        self.key: str = None
-        self.calibration_data: Array = None
 
     def set_key(self, key):
         """
@@ -33,12 +16,51 @@ class LogLogHalfNormalTarget(Target):
         self.key = key
 
     def set_calibration_data(self, data):
-        self.calibration_data = data
-        self._log_data = jnp.log(data)
+        raise NotImplementedError
 
     def loglikelihood(self, modelled):
-        log_result = jnp.log(modelled)
-        log_target = self._log_data
-        dispersion = numpyro.sample(f"dispersion_{self.key}", dist.HalfNormal(self.sd))
-        like_component = dist.Normal(log_result, dispersion).log_prob(log_target).sum()
+        raise NotImplementedError
+
+
+class UnivariateDispersionTarget(Target):
+    def __init__(
+        self,
+        data: pd.Series,
+        dist: dist.Distribution,
+        dispersion_dist: dist.Distribution,
+        dispersion_args: dict,
+        log: bool,
+    ):
+        self.data = data
+        self.dist = dist
+        self.dispersion_dist = dispersion_dist
+        self.dispersion_args = dispersion_args
+
+        self.log = log
+
+        self.key: str = None
+        self.calibration_data: Array = None
+
+    def set_calibration_data(self, data):
+        self.calibration_data = data
+        if self.log:
+            self._log_data = jnp.log(data)
+
+    def loglikelihood(self, modelled):
+        if self.log:
+            result = jnp.log(modelled)
+            target = self._log_data
+        else:
+            result = modelled
+            target = self.calibration_data
+
+        dispersion = numpyro.sample(
+            f"dispersion_{self.key}", self.dispersion_dist(**self.dispersion_args)
+        )
+        like_component = self.dist(result, dispersion).log_prob(target).sum()
         return like_component
+
+
+class StandardTarget(UnivariateDispersionTarget):
+    def __init__(self, data, dispersion_sd=0.1):
+        super().__init__(data, dist.Normal, dist.HalfNormal, {"scale": dispersion_sd}, log=True)
