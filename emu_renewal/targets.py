f666e645fb91a95247c0ea81e60776cc1fc8add2
David Shipman <dshipman@gmail.com>
2024-10-22 11:17:51 +1100

    Refactor target heirarchy and include fixed targets

--- emu_renewal/targets.py
+++ emu_renewal/targets.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Union
 import pandas as pd
 from jax import Array, numpy as jnp
 from numpyro import distributions as dist
@@ -6,11 +6,14 @@ from numpyro.distributions.distribution import DistributionMeta
 import numpyro
 
 
+Transform = Union[Callable | None]
+DispersionSpec = Union[dist.Distribution, float]
+
+
 class Target:
     data: pd.Series
     key: str
     calibration_data: Array
-    transform: Callable
 
     def set_key(self, key):
         """
@@ -19,21 +22,38 @@ class Target:
         self.key = key
 
     def set_calibration_data(self, data):
-        self.calibration_data = self.transform(data) if self.transform else data
+        self.calibration_data = data
 
     def loglikelihood(self, modelled):
         raise NotImplementedError
 
 
-class UnivariateDispersionTarget(Target):
+class TransformTarget(Target):
+    _transform: Transform
+
+    def __init__(self, data: pd.Series, transform: Transform = None):
+        self.data = data
+        self._transform = transform
+
+    def set_calibration_data(self, data):
+        self.calibration_data = self.transform(data)
+
+    def transform(self, x):
+        if self._transform is None:
+            return x
+        else:
+            return self._transform(x)
+
+
+class UnivariateDispersionTarget(TransformTarget):
     def __init__(
         self,
         data: pd.Series,
         dist: DistributionMeta,
-        dispersion_dist: dist.Distribution,
-        transform: callable=None,
+        dispersion: DispersionSpec,
+        transform: Transform = None,
     ):
-        """Create a Target with any distribution, which is parameterised by 
+        """Create a Target with any distribution, which is parameterised by
         the modelled data and parameters to the dispersion distribution.
 
         Args:
@@ -41,24 +61,37 @@ class UnivariateDispersionTarget(Target):
             dist: The likelihood distribution
             dispersion_dist: A dispersion distribution
         """
+
+        super().__init__(data, transform)
+
         self.data = data
         self.dist = dist
-        self.dispersion_dist = dispersion_dist
+        self.dispersion = dispersion
         self.transform = transform
         self.key: str = None
         self.calibration_data: Array = None
 
     def loglikelihood(self, modelled):
         result = self.transform(modelled)
-        dispersion = numpyro.sample(f"dispersion_{self.key}", self.dispersion_dist)
+
+        if isinstance(self.dispersion, dist.Distribution):
+            dispersion = numpyro.sample(f"dispersion_{self.key}", self.dispersion)
+        else:
+            dispersion = self.dispersion
+
         return self.dist(result, dispersion).log_prob(self.calibration_data).sum()
 
 
-class StandardTarget(UnivariateDispersionTarget):
-    def __init__(self, data, dispersion_sd: float):
+class HalfNormalDispTarget(UnivariateDispersionTarget):
+    def __init__(self, data: pd.Series, dispersion_sd: float):
         super().__init__(data, dist.Normal, dist.HalfNormal(dispersion_sd), jnp.log)
 
 
 class UniformDispTarget(UnivariateDispersionTarget):
-    def __init__(self, data, disp_low, disp_high):
+    def __init__(self, data: pd.Series, disp_low: float, disp_high: float):
         super().__init__(data, dist.Normal, dist.Uniform(disp_low, disp_high), jnp.log)
+
+
+class FixedDispTarget(UnivariateDispersionTarget):
+    def __init__(self, data, dispersion: float):
+        super().__init__(data, dist.Normal, dispersion, jnp.log)
