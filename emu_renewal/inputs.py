e4ff655341207c50d21fabe9564ecfd192b1ff0e
James Trauer <james.trauer@monash.edu>
2024-12-11 10:56:14 +1100

    Multi-country variant data loading

--- emu_renewal/inputs.py
+++ emu_renewal/inputs.py
@@ -3,6 +3,11 @@ from pathlib import Path
 
 PROJECT_PATH = Path.cwd().resolve()
 DATA_PATH = PROJECT_PATH.parent / "data"
+VAR_MAP = {
+    "ba1": "21K.Omicron",
+    "ba2": "21L.Omicron",
+    "ba5": "22B.Omicron",
+}
 
 
 def get_indicator_series_from_who_data(indicator, country):
@@ -12,6 +17,11 @@ def get_indicator_series_from_who_data(indicator, country):
     return select_data[indicator].interpolate(method="linear").fillna(0.0)
 
 
+def get_multicountry_df_from_who_data(indicator, countries):
+    data_dict = {i: get_indicator_series_from_who_data(indicator, i) for i in countries}
+    return pd.DataFrame(data_dict)
+
+
 def get_hosp_series_from_owid_data(indicator, country):
     hosp = pd.read_csv(DATA_PATH / "owid/owid_hosp.csv", index_col="date")
     hosp.index = pd.to_datetime(hosp.index)
@@ -19,6 +29,15 @@ def get_hosp_series_from_owid_data(indicator, country):
     return data.loc[data["indicator"] == indicator, "value"]
 
 
-def get_multicountry_df_from_who_data(indicator, countries):
-    data_dict = {i: get_indicator_series_from_who_data(indicator, i) for i in countries}
-    return pd.DataFrame(data_dict)
+def get_var_country_data(var, country):
+    data = pd.read_json(DATA_PATH / "nextclade" / f"{var}.json")[country]
+    dates = pd.to_datetime(data["week"])
+    return pd.Series(data["cluster_sequences"], index=dates)
+
+
+def get_multivars_country_data(var_map, country):
+    return pd.DataFrame({k: get_var_country_data(v, country) for k, v in var_map.items()})
+
+
+def get_row_proportions(df):
+    return df.divide(df.sum(axis=1), axis=0).fillna(0.0)
