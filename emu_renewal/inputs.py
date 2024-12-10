f9503085d8dafeaadc751370e76cdde4e43036c5
James Trauer <james.trauer@monash.edu>
2024-12-11 09:49:15 +1100

    Move WHO data into WHO folder not Australia-specific

--- emu_renewal/inputs.py
+++ emu_renewal/inputs.py
@@ -2,11 +2,11 @@ import pandas as pd
 from pathlib import Path
 
 PROJECT_PATH = Path.cwd().resolve()
-DATA_PATH = PROJECT_PATH.parent / "data/covid_aus"
+DATA_PATH = PROJECT_PATH.parent / "data"
 
 
 def get_indicator_series_from_who_data(indicator, country):
-    who_data = pd.read_csv(DATA_PATH / "WHO-COVID-19-global-data_21_8_24.csv")
+    who_data = pd.read_csv(DATA_PATH / "who/WHO-COVID-19-global-data_21_8_24.csv")
     select_data = who_data.loc[who_data["Country"] == country]
     select_data.index = pd.to_datetime(select_data["Date_reported"], format="%d/%m/%Y")
     return select_data[indicator].interpolate(method="linear").fillna(0.0)
