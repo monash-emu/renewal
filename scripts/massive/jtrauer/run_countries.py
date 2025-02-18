ee0b202b12ac6e9ab98e6e1def61903d6e4a5f1a
jtrauer <james.trauer@monash.edu>
2025-02-18 21:38:19 +1100

    Move runner file to scripts folder

---
+++ scripts/massive/jtrauer/run_countries.py
@@ -0,0 +1,18 @@
+import json
+import sys
+
+from emu_renewal.inputs import DATA_PATH, ANALYSIS_TYPES
+from emu_renewal.run import run_single_country
+
+
+COUNTRIES = ["CZE", "ITA", "DEU", "LTU"]
+
+
+if __name__=="__main__":
+    initial_countries = json.load(open(DATA_PATH / f"config/countries.json", "r"))
+    all_countries = initial_countries["admissions"] + initial_countries["occupancy"]
+    array_task_id = int(sys.argv[2])
+    country = COUNTRIES[array_task_id - 1]  # Convert to Python indexing
+    hosp_out, hosp_out_name = ("Daily hospital occupancy", "occupancy") if country in initial_countries["occupancy"] else ("Weekly new hospital admissions", "admissions")
+    for mob_analysis_type in ANALYSIS_TYPES:
+        run_single_country(country, 10, 7, 50, mob_analysis_type, 100, hosp_out, hosp_out_name)
