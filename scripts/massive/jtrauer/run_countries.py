dca1a198f28fa2dac328decae72ee8f298f2429a
jtrauer <james.trauer@monash.edu>
2025-02-19 16:54:00 +1100

    Revert change to date naming, seems to be working

--- scripts/massive/jtrauer/run_countries.py
+++ scripts/massive/jtrauer/run_countries.py
@@ -6,11 +6,11 @@ from emu_renewal.inputs import DATA_PATH, ANALYSIS_TYPES, DATE_FORMAT
 from emu_renewal.run import run_single_country
 
 
+initial_countries = json.load(open(DATA_PATH / f"config/countries.json", "r"))
+all_countries = initial_countries["admissions"] + initial_countries["occupancy"]
+analysis_name = f"run_{datetime.now().strftime(DATE_FORMAT)}"
+
 if __name__=="__main__":
-    initial_countries = json.load(open(DATA_PATH / f"config/countries.json", "r"))
-    all_countries = initial_countries["admissions"] + initial_countries["occupancy"]
-    extension = "a"
-    analysis_name = f"run_{datetime.now().strftime('%Y%m%d')}_{extension}"
     array_task_id = int(sys.argv[2])
     country = all_countries[array_task_id - 1]  # Convert to Python indexing
     hosp_out, hosp_out_name = ("Daily hospital occupancy", "occupancy") if \
