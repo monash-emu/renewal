96b93a0d876ff8fa40ce02af527d5464b73ce3f2
James Trauer <james.trauer@monash.edu>
2025-03-26 16:01:14 +1100

    Prepare to run 89 countries

--- scripts/massive/jtrauer/run_countries.py
+++ scripts/massive/jtrauer/run_countries.py
@@ -6,11 +6,10 @@ from emu_renewal.run import run_single_country, MobilityException, log
 
 
 if __name__ == "__main__":
-    countries = json.load(open(DATA_PATH / f"config/countries.json", "r"))
-    all_countries = countries["admissions"] + countries["occupancy"]
+    countries = json.load(open(DATA_PATH / f"config/included.json", "r"))
     task_name = sys.argv[1]
     array_task_id = int(sys.argv[2])
-    c = all_countries[array_task_id - 1]  # Convert to Python indexing
+    c = countries[array_task_id - 1]  # Convert to Python indexing
     for mob_type in ANALYSIS_TYPES:
         try:
             run_single_country(c, 7, 50, mob_type, 1000, 50, task_name, num_chains=8)
