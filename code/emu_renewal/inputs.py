d36086464f3399ac24a9b3a3af083ff68f6740bb
James Trauer <james.trauer@monash.edu>
2025-03-31 13:27:50 +1100

    Patch for serosurveys on repeated date for Mexico

--- code/emu_renewal/inputs.py
+++ code/emu_renewal/inputs.py
@@ -241,7 +241,12 @@ def get_filtered_seroprev(
     unity_filt = data["is_unity_aligned"] == "Unity-Aligned"
     n_filt = data["denominator_value"] > 599
     all_filt = time_filt & country_filt & nat_filt & type_filt & unity_filt & n_filt
-    return data.loc[all_filt, "serum_pos_prevalence"]
+    filtered_data = data.loc[all_filt, "serum_pos_prevalence"]
+    if filtered_data.index.has_duplicates:
+        # This is a quick way to drop two of three observations for Mexico that were
+        # on the same date - keeping the first survey, which was was by far the largest
+        filtered_data = filtered_data[[not i for i in filtered_data.index.duplicated()]]
+    return filtered_data
 
 
 def get_standard_priors() -> Dict[str, dist.Distribution]:
