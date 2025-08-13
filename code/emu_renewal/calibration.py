50b1d6c01732d1eb4fa1e02d1e682babc0b8034a
jtrauer <james.trauer@monash.edu>
2025-08-13 16:41:53 +1000

    Give up on cross-referencing tables and just divide up into sections

--- code/emu_renewal/calibration.py
+++ code/emu_renewal/calibration.py
@@ -75,7 +75,8 @@ class StandardCalib:
         Notes
         -----
         For all epidemiological parameters,
-        the priors were set as described in @tbl-durs and @tbl-props.
+        the priors were set as described in the 
+        parameters choices section of this document.
         The dispersion parameter for the variable process
         was set to a half normal distribution with 
         standard deviation {PROC_DISP_SD}.
