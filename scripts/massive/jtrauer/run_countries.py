83868d3814ae04e18de0b00f61127a407e28448f
James Trauer <james.trauer@monash.edu>
2025-09-02 13:02:27 +1000

    Prepare for another full run

--- scripts/massive/jtrauer/run_countries.py
+++ scripts/massive/jtrauer/run_countries.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     country_path.mkdir(parents=True, exist_ok=True)
     logger = get_logger(country_path / "run.log")
     cont = get_cont_of_country(c)
-    analyses = ANALYSIS_TYPES + ["fb_no_mob"] if cont == "OC" else ANALYSIS_TYPES
+    analyses = ANALYSIS_TYPES + ["fb_no_mob"] if cont == "OC" and c != "SGP" else ANALYSIS_TYPES
     for mob_type in analyses:
         try:
             run_single_country(c, mob_type, task, logger=logger)
