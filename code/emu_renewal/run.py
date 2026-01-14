2ce8ffb4b07964ca9db6d543aa7a5852b54dd5ed
James Trauer <james.trauer@monash.edu>
2026-01-14 11:45:26 +1100

    Add analyses file and remove double-mobility exception loop

--- code/emu_renewal/run.py
+++ code/emu_renewal/run.py
@@ -358,9 +358,8 @@ def run_single_country(
     # Mobility
     try:
         mob_provider = get_mobility_provider(iso3, mob_source)
-    except Exception as e:
-        msg = f"{mob_source} mobility not available"
-        raise MobilityException(msg)
+    except MobilityException as e:
+        logger.warning(e)
     if mob_provider.mob_end:
         end_time = min([end_time, mob_provider.mob_end])
 
--- scripts/massive/jtrauer/run_countries.py
+++ scripts/massive/jtrauer/run_countries.py
@@ -2,27 +2,19 @@ import json
 import sys
 
 from emu_renewal.inputs import DATA_PATH
-from emu_renewal.constants import ANALYSIS_TYPES, BASE_PATH
-from emu_renewal.run import (
-    run_single_country,
-    MobilityException,
-    get_logger,
-    jax_config_cpu_only,
-)
-from emu_renewal.utils import get_cont_of_country
+from emu_renewal.constants import BASE_PATH
+from emu_renewal.run import run_single_country, get_logger, jax_config_cpu_only
 
 
 if __name__ == "__main__":
     jax_config_cpu_only()
-    countries = json.load(open(DATA_PATH / f"config/included.json", "r"))
+    countries = json.load(open(DATA_PATH / "config/included.json", "r"))
+    analyses = json.load(open(DATA_PATH / "config/analyses.json", "r"))
     task = sys.argv[1]
     array_task_id = int(sys.argv[2])
     iso3 = countries[array_task_id - 1]  # Convert to Python indexing
     country_path = BASE_PATH / "outputs" / task / iso3
     country_path.mkdir(parents=True, exist_ok=True)
     logger = get_logger(country_path / "run.log")
-    for mob_type in ANALYSIS_TYPES:
-        try:
-            run_single_country(iso3, mob_type, task, False, logger=logger)
-        except MobilityException as e:
-            logger.warning(e)
+    for mob_type in analyses:
+        run_single_country(iso3, mob_type, task, False, logger=logger)
