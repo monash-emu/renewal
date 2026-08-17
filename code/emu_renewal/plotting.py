352d33a829ebc633ed062b1d3541538764b0f57f
James Trauer <james.trauer@monash.edu>
2026-08-17 12:37:23 +1000

    Reinstate code for policy comparison versus residual

--- code/emu_renewal/plotting.py
+++ code/emu_renewal/plotting.py
@@ -715,8 +715,9 @@ def compare_proc_pol(
         plt.setp(ax.xaxis.get_majorticklabels(), rotation=70)
 
         # Transmission scaling process plotting
-        ref_analysis = "fb_no_mob" if (job_path / iso3 / "fb_no_mob").exists() else "no_mob"
-        proc_samples = pd.read_hdf(job_path / iso3 / ref_analysis / "spaghetti.h5")["process"]
+        path = job_path[iso3]
+        ref_analysis = "fb_no_mob" if "fb_no_mob" in path else "no_mob"
+        proc_samples = pd.read_hdf(job_path[iso3][ref_analysis] / "spaghetti.h5")["process"]
         centiles = proc_samples.quantile([0.025, 0.5, 0.975], axis=1).T
         ax.plot(centiles.index, centiles[0.5], color="navy", linewidth=2.0)
         ax.fill_between(centiles.index, centiles[0.025], centiles[0.975], alpha=0.1, color="navy")
