4c513a7029e01cb5084f2a873f204f9a867bafa2
James Trauer <james.trauer@monash.edu>
2026-09-15 14:18:02 +1000

    Add last SHA to valid SHA list

--- code/emu_renewal/constants.py
+++ code/emu_renewal/constants.py
@@ -270,7 +270,7 @@ SHORT_COUNTRY_NAMES = {"Russian Federation": "Russian Fed", "Dominican Republic"
 
 # Run identifiers
 FULL_RUN = ["59824578", "59746206", "59597639"]
-CURRENT_METHOD_SHAS = {"cb8324a", "b934196"}
+CURRENT_METHOD_SHAS = ["cb8324a", "b934196", "048722d"]
 MIN_IDATA_BYTES = 10_000_000
 
 # Stringency
